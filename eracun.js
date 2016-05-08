f0764d317d37d2117b9f5fa993145dae73c4ba0d
Tomaz Vesel <tomaz.vesel95@gmail.com>
2016-05-08 15:31:37 +0000

    Zaključek dela na I. nalogi

--- eracun.js
+++ eracun.js
@@ -211,11 +211,26 @@ streznik.post('/prijava', function(zahteva, odgovor) {
       //TODO: add fields and finalize
       //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
       //stmt.finalize();
+      
+      stmt.run(polja.FirstName, polja.LastName, polja.Company, polja.Address, polja.City, polja.State, polja.Country, polja.PostalCode, polja.Phone, polja.Fax, polja.Email, 3); 
+      stmt.finalize();
+      
     } catch (err) {
       napaka2 = true;
     }
-  
-    odgovor.end();
+    
+    vrniStranke(function(napaka3, stranke) {
+        vrniRacune(function(napaka4, racuni) {
+          var obvestilo = "";
+          if (napaka1 || napaka2 || napaka3 || napaka4) {
+            obvestilo = "Prišlo je do napake pri registraciji nove stranke. Prosim preverite vnešene podatke in poskusite znova.";
+          } else {
+            obvestilo = "Stranka je bila uspešno registrirana.";
+          }
+          odgovor.render('prijava', {sporocilo: obvestilo, seznamStrank: stranke, seznamRacunov: racuni});
+       }) 
+      });
+    
   });
 })
 
@@ -223,8 +238,8 @@ streznik.post('/prijava', function(zahteva, odgovor) {
 streznik.get('/prijava', function(zahteva, odgovor) {
   vrniStranke(function(napaka1, stranke) {
       vrniRacune(function(napaka2, racuni) {
-        odgovor.render('prijava', {sporocilo: "", seznamStrank: stranke, seznamRacunov: racuni});  
-      }) 
+        odgovor.render('prijava', {sporocilo: "", seznamStrank: stranke, seznamRacunov: racuni});
+      })
     });
 })
 
